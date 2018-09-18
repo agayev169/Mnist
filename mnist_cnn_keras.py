6da63f9b62c5f41d17dca872d6d42c2a97bb3c38
agayev169 <agayev169@gmail.com>
2018-09-19 01:23:00 +0400

    Adding new files

--- mnist_cnn_keras.py
+++ mnist_cnn_keras.py
@@ -1,40 +1,41 @@
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Activation, Flatten, Dropout
-from tensorflow.keras.layers import Conv2D, MaxPooling2D
+from tensorflow.keras.layers import Dense, Conv2D, MaxPooling2D, Activation, Flatten, Dropout
+import numpy as np
 
-mnist = tf.keras.datasets.mnist
+from tensorflow.keras.datasets import mnist
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
 
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
+X_train = X_train.reshape(X_train.shape[0], 28, 28, 1)
+X_test = X_test.reshape(X_test.shape[0], 28, 28, 1)
 
-x_train = x_train / 255.0
-x_test = x_test / 255.0
+X_train = X_train.astype('float32')
+X_test = X_test.astype('float32')
+X_train /= 255
+X_test /= 255
 
-x_train = tf.reshape(x_train, shape = [-1, 28, 28, 1])
-x_test = tf.reshape(x_test, shape = [-1, 28, 28, 1])
+Y_train = tf.keras.utils.to_categorical(y_train, 10)
+Y_test = tf.keras.utils.to_categorical(y_test, 10)
 
 model = Sequential()
 
-model.add(Conv2D(1, (2, 2), input_shape = (28, 28, 1)))
+model.add(Conv2D(32, (2, 2), input_shape = (28, 28, 1)))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size = (2, 2)))
 
 model.add(Conv2D(32, (2, 2)))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size = (2, 2)))
-model.add(Dropout(0.1))
+model.add(Dropout(0.2))
 
 model.add(Flatten())
-model.add(Dense(512))
+model.add(Dense(128))
 model.add(Activation('relu'))
-model.add(Dropout(0.1))
+model.add(Dropout(0.2))
 
 model.add(Dense(10))
 model.add(Activation('softmax'))
 
-model.compile(optimizer = 'adam', 
-	loss = 'sparse_categorical_crossentropy',
-	metrics = ['accuracy'])
-
-model.fit(x_train, y_train, epochs = 20, steps_per_epoch = x_train.shape[0] // 128, validation_data = (x_test, y_test))
-# model.evaluate(x_test, y_test)
+model.compile(optimizer = 'adam', loss = 'sparse_categorical_crossentropy', metrics = ['accuracy'])
+model.fit(X_train, y_train, epochs = 20, batch_size = 128, 
+	validation_data = (X_test, y_test))
